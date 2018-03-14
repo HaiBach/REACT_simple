676d11af08b8a91e3ae6b497d15aa983fdda3a48
HaiBach <coder.haibach@gmail.com>
2018-03-14 23:50:49 +0700

    End day: 14/03/2018

--- js/todo.js
+++ js/todo.js
@@ -2,29 +2,45 @@
 class MyTodoList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { todo : [] };
+    this.state = { todo : [], itemCur: '' };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(e) {
-    console.log(e, this);
+    this.setState({ itemCur: e.target.value });
   }
   handleSubmit(e) {
     console.log('Submit page');
+    let todo = this.state.todo;
+    todo.push(this.state.itemCur);
+    this.setState({ todo: todo });
+    e.preventDefault();
   }
 
   render() {
     return (
       <div>
         <h1>Todo list :</h1>
-        <ul>
-        </ul>
+        <TodoItems items={this.state.todo} />
         <form>
           <label>Input new list</label>
           <input type="text" onChange={(e) => this.handleChange(e)} />
           <input type="submit" onClick={this.handleSubmit}/>
         </form>
       </div>
+    );
+  }
+}
+
+class TodoItems extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <ul>
+        {this.props.items.map((item) => <li>{item}</li>)}
+      </ul>
     );
   }
 }
