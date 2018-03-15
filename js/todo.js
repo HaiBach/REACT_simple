082cb7d7a823abafef777befa05eeb860128a4ef
Nguyễn Văn Thy <van_thy@wds.com.vn>
2018-03-15 11:55:26 +0700

    update todo.js

--- js/todo.js
+++ js/todo.js
@@ -2,29 +2,53 @@
 class MyTodoList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { todo : [] };
+    this.state = { todo: [], text: '', id: 1 };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(e) {
-    console.log(e, this);
+    this.setState({ text: e.target.value });
   }
   handleSubmit(e) {
-    console.log('Submit page');
+    let todo = this.state.todo;
+
+    // Insert new item
+    todo.push(this.state.text);
+
+    this.setState({ todo: todo, text: '', id: this.state.id + 1 });
+    e.preventDefault();
   }
 
   render() {
     return (
       <div>
         <h1>Todo list :</h1>
-        <ul>
-        </ul>
+        <TodoItems items={this.state.todo} />
         <form>
           <label>Input new list</label>
-          <input type="text" onChange={(e) => this.handleChange(e)} />
-          <input type="submit" onClick={this.handleSubmit}/>
+          <input
+            type="text" name="text"
+            onChange={(e) => this.handleChange(e)}
+            value={this.state.text} />
+          <input
+            type="submit"
+            onClick={this.handleSubmit}
+            value={'Add #' + this.state.id } />
         </form>
       </div>
+    );
+  }
+}
+
+class TodoItems extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <ul>
+        { this.props.items.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     );
   }
 }
